1d360a5506651cd22065961ea59e01af0a02bfa1
Alejandro Roby <aleroby@gmail.com>
2025-10-01 19:43:37 +0000

    realizamos asociaciones y validaciones en User, Service, Order y Review

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,9 @@
 class Review < ApplicationRecord
   belongs_to :service
+  belongs_to :client, class_name: "User"
+  belongs_to :supplier, class_name: "User"
+
+  validates :rating, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
+  validates :content, presence: true, length: { minimum: 50 }
+
 end
--- app/models/service.rb
+++ app/models/service.rb
@@ -1,3 +1,14 @@
 class Service < ApplicationRecord
   belongs_to :user
+  has_many :orders
+  has_many :reviews
+
+  CATEGORIAS_PERMITIDAS = []
+  SUB_CATEGORIAS_PERMITIDAS = []
+
+  validates :category, presence: true, inclusion: { in: CATEGORIAS_PERMITIDAS, message: "no es una categoría válida" }
+  validates :sub_category, presence: true, inclusion: { in: SUB_CATEGORIAS_PERMITIDAS, message: "no es una categoría válida" }
+  validates :description, presence: true, length: { minimum: 200 }
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
+
 end
