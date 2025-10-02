044b5b3cd5006bf934092f19e00d21b931c6a738
Alejandro Roby <aleroby@gmail.com>
2025-10-02 21:14:04 +0000

    Creamos el users/Services controller

--- app/models/service.rb
+++ app/models/service.rb
@@ -3,13 +3,14 @@ class Service < ApplicationRecord
   has_many :orders
   has_many :reviews
 
+  has_many_attached :images
 
   CATEGORIAS_PERMITIDAS = []
   SUB_CATEGORIAS_PERMITIDAS = []
 
   validates :category, presence: true, inclusion: { in: CATEGORIAS_PERMITIDAS, message: "no es una categoría válida" }
   validates :sub_category, presence: true, inclusion: { in: SUB_CATEGORIAS_PERMITIDAS, message: "no es una categoría válida" }
-  validates :description, presence: true, length: { minimum: 200 }
+  validates :description, presence: true, length: { minimum: 10 }
   validates :price, numericality: { greater_than_or_equal_to: 0 }
 
 end
