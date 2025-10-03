a2ce5c9c5f4b18e0c3a0ad79c7ff205d1315e0a8
Alejandro Roby <aleroby@gmail.com>
2025-10-03 05:53:21 +0000

    services-index mas encaminada

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,7 +6,10 @@ eagerLoadControllersFrom("controllers", application)
 // app/javascript/controllers/index.js
 import { Application } from "@hotwired/stimulus"
 import DependentSelectController from "./dependent_select_controller"
+import FiltersController from "./filters_controller";
+import PriceRangeController from "./price_range_controller"; // si ya lo tenés
 
 window.Stimulus = Application.start()
 Stimulus.register("dependent-select", DependentSelectController)
-
+Stimulus.register("filters", FiltersController);
+Stimulus.register("price-range", PriceRangeController); // opcional
